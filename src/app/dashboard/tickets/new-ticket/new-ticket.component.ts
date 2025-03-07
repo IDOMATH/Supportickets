e495c1d4a1a11b3a9766db8897142170305c49dd
IDOMATH <btdothemath@gmail.com>
2025-03-07 17:28:38 -0600

    Styling using Host Elements

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ControlComponent } from "../../../shared/control/control.component";
+import { ButtonComponent } from "../../../shared/button/button.component";
 
 @Component({
-  selector: 'app-new-ticket',
+  selector: "app-new-ticket",
   standalone: true,
-  imports: [],
-  templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  imports: [ButtonComponent, ControlComponent],
+  templateUrl: "./new-ticket.component.html",
+  styleUrl: "./new-ticket.component.css",
 })
-export class NewTicketComponent {
-
-}
+export class NewTicketComponent {}
